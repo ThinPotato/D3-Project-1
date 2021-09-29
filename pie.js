6ed6e079e8c6447b76e599f7ed02d6a34a576004
ThinPotato <bryce940@gmail.com>
2021-09-29 14:11:03 -0400

    Pie almost working

--- pie.js
+++ pie.js
@@ -1,39 +1,39 @@
-loadChart(1)
+loadChart(0)
 
 function getDropDownValue(){
-    var ddReference = document.getElementsByClassName("dropdown_content");
+    var ddReference = document.getElementsByClassName('dropdown_content');
     selected = ddReference.options[ddReference.selectedIndex].value;
     return selected;
 }
 var data;
-document.getElementById("element_selector").onchange = function() {selectElement()};
+document.getElementById('element_selector').onchange = function() {selectElement()};
 
 function selectElement(){
     var selected = document.getElementById('element_selector').selectedOptions[0].value;
     console.log('changing to ' + selected)
-    d3.select("svg").remove()
+    d3.select('svg').remove()
     loadChart(selected)
 }
 
 function dataSelector(element, selected){
     console.log('updating data to ' + selected)
     switch (selected) {
-        case "0":
+        case '0':
             return element.Avg_Sleepiness.substring(0,5)
             break;
-        case "1":
+        case '1':
             return element.People_Queried_About_Sleep
             break;
-        case "2":
+        case '2':
             return element.Crashes_per_Year
             break;
-        case "3":
+        case '3':
             return element.Avg_Crash_Severity
             break;
-        case "4":
+        case '4':
             return element.Percent_Morning_Crashes.substring(0,5)
             break;
-        case "5":
+        case '5':
             return element.Percent_Evening_Crashes.substring(0,5)
             break;
         default:
@@ -42,38 +42,48 @@ function dataSelector(element, selected){
 }
 
 function loadChart(selected){
-    var svg = d3.select("body")
-    .append("svg")
-    .attr("width", 30000)
-    .attr("height", 800);
+    var svg = d3.select('body')
+    .append('svg')
+    .attr('width', 800)
+    .attr('height', 800);
     // bar chart
-    d3.csv("Access Assignment 1 Proposal - Bryce Stoker-Schaeffer (11199983).csv").then(function(data){
+
+    //TODO: Parse data here
+    var svg = d3.select('svg'),
+    width = 800,
+    height = 800,
+    radius = Math.min(width, height) / 2 
+    g = svg.append('g').attr('transform', 'translate('+ width / 2 + ',' + height / 2 + ')');
+    var pie = d3.pie()
+
+    var arc = d3.arc().innerRadius(0).outerRadius(radius)
+
+    var path = d3.arc()
+			.outerRadius(radius - 40)
+			.innerRadius(100);
+	var label = d3.arc()
+			.outerRadius(radius)
+			.innerRadius(radius - 150);
+
+    d3.csv('Access Assignment 1 Proposal - Bryce Stoker-Schaeffer (11199983).csv').then(function(data){
         console.log(data)
-        //TODO: Parse data here
-        var svg = d3.select("svg"),
-        margin = 300,
-        width = 2000 - margin,
-        height = svg.attr("height") - margin,
-        xScale = d3.scaleBand().range([0,width]).padding(0.5),
-        yScale = d3.scaleLinear().range([height,0]),
-        radius = Math.min(width, height) / 2 - margin
-        g = svg.append("g").attr("transform", "translate("+100+","+100+")");
-
-        xScale.domain(data.map(function(d){return d.State;}));
-        yScale.domain([0, d3.max(data,function(d){return Number(dataSelector(d,selected));})])
-        g.append("g").attr("transform", "translate(0," + height + ")").call(d3.axisBottom(xScale))
-        g.append("g").call(d3.axisLeft(yScale).tickFormat(function(d){
-            return d + ((selected ==0 || selected == 4 || selected ==5) ? "%" : "");
-        }).ticks(10))
-
-        g
-        .selectAll(".bar")
-        .data(data)
-        .enter()
-        .append("rect")
-        .attr("class","bar")
-        .attr("width", xScale.bandwidth())
-        .attr("height", function(d){return height - yScale(Number(dataSelector(d,selected)));})
+        var arcs = g.selectAll('arc')
+        .data(pie(data.map(function(d){return Number(dataSelector(d,selected));}).slice(0,7)))
+        .enter().append('g')
+        .attr('class','arc')
+        var color = d3.scaleOrdinal(['#e40303','#ff8c00', '#ffed00', '#008026','#004dff','#750787'])
+        console.log(data)
+        arcs.append('path') 
+		.attr('fill',function(d){
+			return color(data.map(function(d){return d.State}));
+		})
+		.attr('d', arc);
+        arcs.append('text')
+			.attr('transform', function(d){return 'translate(' + label.centroid(d) + ')';})
+			.text(function(d){
+                console.log(d)
+                console.log("name "+d.data)
+                return d.data});
     });
 
     d3.select('h3').style('color', 'darkblue');
