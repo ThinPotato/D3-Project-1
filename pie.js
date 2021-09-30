764d2fbefb3717e4a66a306f335347e004286ab6
ThinPotato <bryce940@gmail.com>
2021-09-29 23:22:46 -0400

    Pie chart working. TODO: group other

--- pie.js
+++ pie.js
@@ -1,4 +1,3 @@
-loadChart(0)
 
 function getDropDownValue(){
     var ddReference = document.getElementsByClassName('dropdown_content');
@@ -15,33 +14,52 @@ function selectElement(){
     loadChart(selected)
 }
 
-function dataSelector(element, selected){
-    console.log('updating data to ' + selected)
-    switch (selected) {
-        case '0':
-            return element.Avg_Sleepiness.substring(0,5)
-            break;
-        case '1':
-            return element.People_Queried_About_Sleep
-            break;
-        case '2':
-            return element.Crashes_per_Year
-            break;
-        case '3':
-            return element.Avg_Crash_Severity
-            break;
-        case '4':
-            return element.Percent_Morning_Crashes.substring(0,5)
-            break;
-        case '5':
-            return element.Percent_Evening_Crashes.substring(0,5)
-            break;
-        default:
-            return element.Avg_Sleepiness.substring(0,5)
-    }
+function dataSum(data, selected){
+    temp = d3.group(data, d => d.State)
+    var preData ={};
+    var newData=[];
+    for (const [key, value] of temp) {
+        //console.log(key, value);
+        var State = temp.get(key)[0].State
+        //console.log("Key Value:" + value)
+        preData[value] = temp.get(key)
+        numToAdd = 0;
+        count = 0;
+        for (const [key2, value2] of value.entries()){
+            count +=1;
+            //console.log("Inner Data: "+ value2.People_Queried_About_Sleep)
+            switch (selected){
+                case('0'):
+                    numToAdd += Number(value2.Avg_Sleepiness.substring(0,5))
+                    break;
+                case('1'):
+                    numToAdd += Number(value2.People_Queried_About_Sleep)
+                case('2'):
+                    numToAdd += Number(value2.Crashes_per_Year)
+                    break;
+                case('3'):
+                    numToAdd += Number(value2.Avg_Crash_Severity)
+                case('4'):
+                    numToAdd += Number(value2.Percent_Morning_Crashes.substring(0,5))
+                case('5'):
+                    numToAdd += Number(value2.Percent_Evening_Crashes.substring(0,5))
+                    break;
+                case('default'):
+                    numToAdd += Number(value2.Avg_Sleepiness.substring(0,5))
+            }
+        }
+        if(selected == '0' || selected == '4' || selected == '5'){
+            numToAdd = numToAdd/count
+        }
+        //newData[State] = numToAdd
+        newData.push({State: State, value: numToAdd})
+        console.log("added "+ numToAdd + " to " + State)
+        }
+    return newData
 }
 
 function loadChart(selected){
+    
     var svg = d3.select('body')
     .append('svg')
     .attr('width', 800)
@@ -55,12 +73,12 @@ function loadChart(selected){
     radius = Math.min(width, height) / 2 
     g = svg.append('g').attr('transform', 'translate('+ width / 2 + ',' + height / 2 + ')');
 
-    var pie = d3.pie()
-    .value(function(d){
-        console.log("Key Value: " +d.People_Queried_About_Sleep)
-        console.log("selected key value: " + dataSelector(d))
-        return dataSelector(d, selected)
-    }).sort(null)
+    var pie = d3.pie().value(d => d.value)
+
+    //var pie = d3.pie()
+    //.value(function(d){
+      //  return dataSelector(d, selected)
+    //}).sort(null)
 
 
     var arc = d3.arc().innerRadius(0).outerRadius(radius)
@@ -74,15 +92,21 @@ function loadChart(selected){
 
     d3.csv('Access Assignment 1 Proposal - Bryce Stoker-Schaeffer (11199983).csv').then(function(data){
         console.log(data)
+        //bin1 = d3.bin().value(d => d.State)
+        //temp = bin1(data)
         
+        data = dataSum(data, selected)
+        //console.log(data.map(function(d) {return d[0]}))
+        console.log(data)
         var arcs = g.selectAll('arc')
         .data(pie(data))
         .enter().append('g')
         .attr('class','arc')
         
-        var color = d3.scaleOrdinal(['#e40303','#ff8c00', '#ffed00', '#008026','#004dff','#750787'])
-        console.log(data)
-        
+        var color = d3.scaleOrdinal()
+        .domain(data.map(d => d.name))
+        .range(d3.quantize(t => d3.interpolateSpectral(t * 0.9 + 0.1), data.length).reverse())
+          
         arcs.append('path') 
 		.attr('fill',function(d){
 			return color(d.data.State);
@@ -98,3 +122,4 @@ function loadChart(selected){
     d3.select('h3').style('font-size', '24px');
 }
 
+loadChart(0)
